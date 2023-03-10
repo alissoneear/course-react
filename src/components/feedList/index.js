d6d2c4d6a2bbc05f4aa5b0a154f7f4abae36db13
alissoneear <alissoneear@gmail.com>
2023-03-10 17:34:43 -0300

    new components

---
+++ src/components/feedList/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const FeedList = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+FeedList.propTypes = {
+
+};
+
+export default FeedList;
