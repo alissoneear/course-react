d195e5e5d3494ac352b46734c42d72d1629f8ebf
alissoneear <alissoneear@gmail.com>
2023-03-11 21:11:45 -0300

    put the ExpenseDate components and CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
-import IncreaseDecreaseButton from "./components/IncreaseDecreaseButton";
-import CycleLife from "./components/CycleLife";
-import Member from "./components/Member";
-import FeedList from "./components/FeedList";
+import IncreaseDecreaseButton from "./components/BRCourse/IncreaseDecreaseButton";
+import CycleLife from "./components/BRCourse/CycleLife";
+import Member from "./components/BRCourse/Member";
+import FeedList from "./components/BRCourse/FeedList";
 import ExpenseItem from "./components/ExpenseItem";
 
 export default class App extends Component {
+    state = {
+        expenses: [
+            {
+                id: 'e1',
+                title: 'Toilet Paper',
+                amount: 94.12,
+                date: new Date(2020, 7, 14),
+            },
+            {
+                id: 'e2',
+                title: 'New TV',
+                amount: 799.49,
+                date: new Date(2021, 2, 12),
+            },
+            {
+                id: 'e3',
+                title: 'Car Insurance',
+                amount: 294.67,
+                date: new Date(2021, 2, 28),
+            },
+            {
+                id: 'e4',
+                title: 'New Desk (Wooden)',
+                amount: 450,
+                date: new Date(2021, 5, 12),
+            },
+        ]
+    }
     render() {
         return (
             <div>
@@ -17,7 +45,26 @@ export default class App extends Component {
                 <hr />
                 <FeedList />
                 <hr />
-                <ExpenseItem />
+                <ExpenseItem
+                    title={this.state.expenses[0].title}
+                    amount={this.state.expenses[0].amount}
+                    date={this.state.expenses[0].date}
+                />
+                <ExpenseItem
+                    title={this.state.expenses[1].title}
+                    amount={this.state.expenses[1].amount}
+                    date={this.state.expenses[1].date}
+                />
+                <ExpenseItem
+                    title={this.state.expenses[2].title}
+                    amount={this.state.expenses[2].amount}
+                    date={this.state.expenses[2].date}
+                />
+                <ExpenseItem
+                    title={this.state.expenses[3].title}
+                    amount={this.state.expenses[3].amount}
+                    date={this.state.expenses[3].date}
+                />
             </div>
         );
     }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
 import './ExpenseItem.css'
+import './ExpenseDate'
+import ExpenseDate from './ExpenseDate';
 
-export default class ExpenseItem extends Component {
-    render() { 
-        return (
-            <div className="expense-item">
-                <div>March 28th 2021</div>
-                <div className="expense-item__description">
-                    <h2>Car Insurance</h2>
-                    <div className='expense-item__price'>$294.67</div>
-                </div>
+export default function ExpenseItem(props) {
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
-        );
-    }
+        </div>
+    );
 }
